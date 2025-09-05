0f26fde07e6239a233643759763c200211044d3a
jo <johyun0218@naver.com>
2025-09-05 10:07:28 +0900

    jar 실행으로 변경

--- README.md
+++ README.md
@@ -25,3 +25,18 @@ venv\Scripts\activate # 맥) source venv/bin/activate
 pip install -r requirements.txt
 
 ```
+
+
+### 프린트 찾기 성공
+
+wmic printer get name,portname
+
+copy app.py  "\\NucBoxG2\Argox CP-2140EX-USB"
+
+N
+q400
+Q200,24
+b20,20,0,4,2,2,80,M,"1234567890"
+A150,20,0,3,1,1,N,"홍길동1"
+A150,60,0,3,1,1,N,"{{date}}"
+P1
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 import socket
 import os
+import subprocess
 
 app = Flask(__name__)
 
 LABEL_TEMPLATE = "labels/asset-template.prn"
-PRINTER_NAME = "Argox"  # 윈도우 제어판에 표시된 프린터 이름
+PRINTER_NAME = "Argox CP-2140EX-USB"  # 윈도우 제어판에 표시된 프린터 이름
 PRN_OUTPUT = "labels/asset-output.prn"
 
 
@@ -39,7 +40,7 @@ def print_label(name: str, date: str, qr: str):
 
     # 4) 프린터로 전송
     # --- (A) USB 프린터 (윈도우 공유 프린터 방식) ---
-    os.system(f'COPY /B "{PRN_OUTPUT}" "\\\\localhost\\{PRINTER_NAME}"')
+    os.system(f'COPY /B "{PRN_OUTPUT}" "\\\\NucBoxG2\\{PRINTER_NAME}"')
 
     # --- (B) 네트워크 프린터 (주석 해제해서 사용) ---
     # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -47,17 +48,43 @@ def print_label(name: str, date: str, qr: str):
     # sock.sendall(content.encode("utf-8"))
     # sock.close()
 
+def print_jar(device_name: str, mng_data: str, product_data: str, purchase_data: str, qr: str):
+    # print("print jar called")
+    # 실행할 JAR 파일
+    jar_file = "qooi-print.jar"
+
+    printer_logger = "Y" # "Y"로 설정하면 qooi-print.jar 내부에서 로그를 logs/qooi-print.log에 기록
+
+    # 파라미터 (예: arg1 arg2)
+    params = [device_name, mng_data, product_data, purchase_data, qr, printer_logger]
+
+    # 실행 명령어
+    cmd = ["java", "-jar", jar_file] + params
+
+    # print("Executing command:", " ".join(cmd))
+
+    # 실행
+    result = subprocess.run(cmd, 
+                            capture_output=True, 
+                            text=True,          # stdout/stderr을 str로 받음
+                            encoding="cp949")    # 출력 인코딩 강제 (윈도우에서 cp949 필요할 수 있음))  linux: utf-8
+    
+
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
 
 @app.route("/print", methods=["POST"])
 def print_api():
     """라벨 출력 API"""
     data = request.json
-    name = data.get("name", "")
-    date = data.get("date", "")
+    device_name = data.get("device_name", "")
+    mng_data = data.get("mng_data", "")
+    product_data = data.get("product_data", "")
+    purchase_data = data.get("purchase_data", "")
     qr = data.get("qr", "")
 
     try:
-        print_label(name, date, qr)
+        print_jar(device_name, mng_data, product_data, purchase_data, qr)
         return jsonify({"status": "ok", "msg": "라벨 출력 완료"})
     except Exception as e:
         return jsonify({"status": "error", "msg": str(e)}), 500
